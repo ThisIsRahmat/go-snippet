04198886f62e841cba8c0c45a5914db7bafc3637
Temiloluwa Samuel <65780579+stemitom@users.noreply.github.com>
2022-07-28 07:39:13 +0100

    Added code to new project structure

---
+++ cmd/web/handlers.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	_, err := w.Write([]byte("Hello from SnippetBox"))
+	if err != nil {
+		return
+	}
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	_, err = fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+	if err != nil {
+		return
+	}
+}
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", 405)
+		return
+	}
+	_, err := w.Write([]byte("Create a new snippet..."))
+	if err != nil {
+		return
+	}
+}
