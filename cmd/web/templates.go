15a1c476647fb3ef07162b3d7a0ae94dd5c4262d
Temiloluwa Samuel <65780579+stemitom@users.noreply.github.com>
2022-08-04 06:55:54 +0100

    Added forms package for cleaner validation rules

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/stemitom/go-snippet/pkg/forms"
 	"github.com/stemitom/go-snippet/pkg/models"
 	"html/template"
 	"path/filepath"
@@ -9,6 +10,7 @@ import (
 
 type templateData struct {
 	CurrentYear int
+	Form        *forms.Form
 	Snippet     *models.Snippet
 	Snippets    []*models.Snippet
 }
